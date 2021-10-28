36e9d408c6d8592be964dbb1274a87d1bfadffce
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-29 01:30:38 +0600

    first commit

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import initializeAuthentication from "../Firebase/firebase.init";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, signOut } from "firebase/auth";
+
+
+initializeAuthentication();
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user)
+            }
+            else {
+                setUser({});
+            }
+            setIsLoading(false);
+        })
+        return () => unsubscribe();
+    }, []);
+
+
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
+
+    }
+
+    const createAccountWithEmail = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const loginWithEmailAndPassword = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () => {
+        signOut(auth)
+            .then(() => {
+                setUser({});
+            }).catch((error) => {
+                console.log(error);
+            });
+    }
+
+    const updateName = (name) => {
+        updateProfile(auth.currentUser, {
+            displayName: name
+        }).then(() => {
+            const newUser = { ...user, displayName: name };
+            setUser(newUser);
+        }).catch((error) => {
+            // An error occurred
+            // ...
+        });
+    }
+
+
+    return {
+        user,
+        setUser,
+        signInWithGoogle,
+        createAccountWithEmail,
+        loginWithEmailAndPassword,
+        isLoading,
+        setIsLoading,
+        updateName,
+        logOut
+    }
+}
+
+export default useFirebase;
