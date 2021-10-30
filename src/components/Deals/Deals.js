80701756aeb396abb4df3c62b2f9a497449f0f47
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-30 12:44:44 +0600

    all deals page done

--- src/components/Deals/Deals.js
+++ src/components/Deals/Deals.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useDeals from '../../hooks/useDeals';
+import Deal from '../Deal/Deal';
 
 const Deals = () => {
+    const { deals } = useDeals();
     return (
         <div>
-            <h2>This is Deals</h2>
+            <h2 className="my-5 display-1 text-dark">All deals</h2>
+            <div className="row g-4 mb-5">
+                {
+                    deals.map(deal => <Deal
+                        key={deal._id}
+                        deal={deal}>
+                    </Deal>)
+                }
+            </div>
         </div>
     );
 };
