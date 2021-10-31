c1d09c267cdce6c7c49de6c729c4eda70ff07942
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-31 17:52:03 +0600

    my bookings done

--- src/components/MyBookings/MyBookings.js
+++ src/components/MyBookings/MyBookings.js
@@ -29,19 +29,17 @@ const MyBookings = () => {
 
     return (
         <div>
-            <h1>Total Bookings on website: {orders.length}</h1>
-            <h2 className="my-5">My Bookings: {myBookings.length}</h2>
-
-            <div className="row g-4">
+            <h2 className="mt-5 display-6 fw-bold">My Bookings: {myBookings.length}</h2>
+            <hr className="w-50 mx-auto mb-5" />
+            <div className="row w-50 mx-auto my-5 g-3 border border-2 border-warning p-5">
                 {myBookings.map(booking => <div key={booking._id} className="col-lg-4 col-md-6 col-sm-12">
                     <div className="img-container">
-                        <img className="w-100" src={booking.itemDetails.img} alt="" />
+                        <img className="w-50" src={booking.itemDetails.img} alt="" />
                     </div>
                     <h3>{booking.itemDetails.name}</h3>
-                    <h6>{booking.itemDetails.destination}</h6>
-
+                    <h5>{booking.itemDetails.destination}</h5>
                     <p>Price: {booking.itemDetails.price}</p>
-                    <button onClick={() => handleCancelBooking(booking._id)} className="btn btn-warning fw-bold"><h3>Cancel Booking</h3></button>
+                    <button onClick={() => handleCancelBooking(booking._id)} className="btn btn-warning fw-bold">Cancel Booking</button>
                 </div>)}
             </div>
         </div>
