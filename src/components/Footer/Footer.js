c395d5c58a215059a6cb3306ae1decc70769f8e5
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-31 19:15:52 +0600

    all done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,54 @@
 import React from 'react';
-import './Footer.css';
-
+import './Footer.css'
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faLinkedin, faTwitterSquare, faYoutube } from '@fortawesome/free-brands-svg-icons';
 const Footer = () => {
+
+    const fbIcon = <FontAwesomeIcon icon={faFacebookSquare} />;
+    const linkedInIcon = <FontAwesomeIcon icon={faLinkedin} />;
+    const twitterIcon = <FontAwesomeIcon icon={faTwitterSquare} />;
+    const youtubeIcon = <FontAwesomeIcon icon={faYoutube} />;
     return (
         <div className="footer-container">
-            <h6 className="display-1 py-5 text-light">This is footer</h6>
+            <div className="mx-4">
+                <div className="row g-4 mx-auto">
+                    <div className="mt-4 col-lg-4 col-md-6 col-sm-12">
+                        <h1 className="text-start text-warning">TravelX</h1>
+                        <p className="text-start text-warning">Travel Save Repeat
+                        </p>
+                        <p className="text-start"><span>Explore the world with us and</span> <br /> <span>make your life happening , adventerous and enjoyable</span></p>
+                        <div className="text-start my-5">
+                            <NavLink className="link-styles" to="/blabla">{fbIcon}</NavLink>
+                            <NavLink className="link-styles" to="/blabla">{linkedInIcon}</NavLink>
+                            <NavLink className="link-styles" to="/blabla">{twitterIcon}</NavLink>
+                            <NavLink className="link-styles" to="/blabla">{youtubeIcon}</NavLink>
+                        </div>
+                    </div>
+                    <div className="mt-4 col-lg-4 col-md-6 col-sm-12">
+
+                        <ul>
+                            <li><h2>Our Services</h2></li>
+                            <li >Custom Tour Packages</li>
+                            <li>Personal Guide</li>
+                            <li>Pickup and Drop off</li>
+                            <li>Accomodation Arragnements</li>
+                        </ul>
+                    </div>
+                    <div className="mt-4 col-lg-4 col-md-6 col-sm-12">
+                        <ul>
+                            <li> <h2>Contact</h2></li>
+                            <li >Address:</li>
+                            <li >Bashundhara R/A, Dhaka</li>
+                            <li>email: ishtiaq.wasi@gmail.com</li>
+                            <li>Phone: 0123456</li>
+                        </ul>
+                    </div>
+                </div>
+                <p>© 2021 TravelX Inc. All rights reserved.</p>
+            </div>
+
+
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import { useHistory } from 'react-router';
+import './NotFound.css';
 const NotFound = () => {
+    const history = useHistory();
+    const handleHomeBtn = () => {
+        history.push('/home');
+
+    }
+
     return (
-        <div>
-            <h1>404</h1>
-            <h4>Not found</h4>
+
+        <div className="error-container">
+            <div className="my-5">
+                <h1 className="display-1 fw- bold ">404</h1>
+                <h4 className="display-2">Not found</h4>
+                <p>The resource requested could not be found on this server!</p>
+                <button onClick={handleHomeBtn} className="btn btn-warning btn-lg my-5">Home</button>
+            </div>
         </div>
     );
 };
